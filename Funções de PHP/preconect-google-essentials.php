c3f9c9042e84cebe2b85e353bb7d9a909fcd25bd
vitor.science <48693281+vitorgalante@users.noreply.github.com>
2025-01-30 13:27:02 -0300

    Update preconect-google-essentials.php
    Adicionada descrição

--- Funções de PHP/preconect-google-essentials.php
+++ Funções de PHP/preconect-google-essentials.php
@@ -1,3 +1,9 @@
+<?php
+/**
+ * Copie o código abaixo e cole-o no arquivo function.php do tema child
+ * Este snippet adiciona uma preconexão com serviços da google na requisição de header da página
+ * versão - 0.1.300125t13
+ */
 function add_preconnect_links() {
     ?>
     <!-- Preconnect para Google Fonts -->
